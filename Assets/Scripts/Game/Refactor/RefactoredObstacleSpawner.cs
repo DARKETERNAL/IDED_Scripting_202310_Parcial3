eed14c0cb8774bb9b34ca26995d1914403831685
jamayav <juan.amayav@upb.edu.co>
2023-05-06 17:46:38 -0500

    algunossingletons

--- Assets/Scripts/Game/Refactor/RefactoredObstacleSpawner.cs
+++ Assets/Scripts/Game/Refactor/RefactoredObstacleSpawner.cs
@@ -11,8 +11,40 @@ public class RefactoredObstacleSpawner : ObstacleSpawnerBase
     [SerializeField]
     private PoolBase obstacleHardPool;
 
+    [SerializeField]
+    private GameObject[] obstaclePrefabs;
+    private static RefactoredObstacleSpawner instance;
+
+    public static RefactoredObstacleSpawner Instance => instance;
+
     protected override void SpawnObject()
     {
         throw new System.NotImplementedException();
     }
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+    private int ObjectIndex
+    {
+        get
+        {
+            int result = 0;
+
+            if (obstaclePrefabs.Length > 1)
+            {
+                result = Random.Range(result, obstaclePrefabs.Length);
+            }
+
+            return result;
+        }
+    }
+
 }
--- Assets/Scripts/Game/Refactor/Singleton/RefactoredUIManager.cs
+++ Assets/Scripts/Game/Refactor/Singleton/RefactoredUIManager.cs
@@ -1,6 +1,28 @@
+using UnityEngine;
 public class RefactoredUIManager : UIManagerBase
 {
     protected override PlayerControllerBase PlayerController => throw new System.NotImplementedException();
 
     protected override GameControllerBase GameController => throw new System.NotImplementedException();
+
+    private static RefactoredUIManager instance;
+
+    public static RefactoredUIManager Instance => instance ??= FindObjectOfType<RefactoredUIManager>();
+
+    [SerializeField]
+    private PlayerController playerController;
+
+    [SerializeField]
+    private GameControllerBase gameController;
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 }
